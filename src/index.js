a556b0faf9b7eb45653f39c4479707e18bdf14e7
Victorsloop <torres.victor136@gmail.com>
2021-04-13 20:55:50 -0400

    Imported broswer and provider and altered indexjs

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Provider from 'react-redux'
+import BrowserRouter from 'react-router-dom'
+const store = createStore( applyMiddleware(thunk))
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <BrowserRouter>
+       <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
