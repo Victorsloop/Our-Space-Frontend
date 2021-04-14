1cde0d3ebeeb14cdb915e4009640e7ca84878a10
Victorsloop <torres.victor136@gmail.com>
2021-04-14 10:18:56 -0400

    Signup basic layout

---
+++ src/Component/Signup.js
@@ -0,0 +1,13 @@
+import React from 'react'
+class Signup extends React.Component{
+    state = {
+
+    }
+    render(){
+        return(
+            <h1>This is the signup page </h1>
+        )
+    }
+
+}
+export default Signup
