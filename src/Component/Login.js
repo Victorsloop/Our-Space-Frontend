11fbcfb3692be1ca37f7efeb60d4d05a7067115c
Victorsloop <torres.victor136@gmail.com>
2021-04-14 10:26:56 -0400

    Login and signup page progress

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -8,7 +8,7 @@ class Login extends React.Component {
         password: ""
     }
 
-    changeHandler = (e) => {
+    inputHandler = (e) => {
         this.setState({ [e.target.name]: e.target.value })
     }
 
@@ -28,15 +28,10 @@ class Login extends React.Component {
             <div class="login-box">
             <form onSubmit = {this.loginHandler} >
             <div class="user-box">
-                <input type="text" name="first_name" placeholder="first_name" value={this.state.first_name} onChange={this.inputHandler} /> 
-                <input type="text" name="last_name" placeholder="last_name" value={this.state.last_name} onChange={this.inputHandler} /> 
-                <input type="text" name="username" placeholder="username" value={this.state.username} onChange={this.inputHandler} /> 
-                <input type="password" name="password" placeholder="password" value={this.state.password} onChange={this.inputHandler}/> 
-                <input type="text" name="hometown" placeholder="Hometown" value={this.state.hometown} onChange={this.inputHandler}/> 
-                <input type="number" name="age" placeholder="Age" value={this.state.age} onChange={this.inputHandler}/> 
-                <input type="number" name="avatar" placeholder="PIC" value={this.state.avatar} onChange={this.inputHandler}/> 
-                <input type="submit" value="Sign Up"/>
-                </div>
+             <input type="text" name="username" placeholder="username" value={this.state.username} onChange={this.inputHandler} /> 
+            <input type="password" name="password" placeholder="password" value={this.state.password} onChange={this.inputHandler}/> 
+            <input type="submit" value="Login"/>
+            </div>
             </form>
             </div>
             </>
--- src/Component/Signup.js
+++ src/Component/Signup.js
@@ -1,7 +1,13 @@
 import React from 'react'
 class Signup extends React.Component{
     state = {
-
+        username: "",
+        password: "",
+        email: "",
+        name: "",
+        pronouns: "",
+        bio: "",
+        avatar: ""
     }
     render(){
         return(
