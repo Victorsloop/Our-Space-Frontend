ccc9b5f0e41cdde367e06c6bcad5dd60e6ff4691
Victorsloop <torres.victor136@gmail.com>
2021-04-15 11:54:06 -0400

    Action types set to const and exported

---
+++ src/Redux/actionTypes.js
@@ -0,0 +1,13 @@
+const actionTypes = {
+    currentUser: "SET_USER",
+    refreshUser: "REFRESH_USER",
+    editAccount: "UPDATE_USER",
+    deleteAccount: "DELETE_USER",
+    browseDillemas: "GET_DILLEMAS",
+    myDillemas: "MY_DILLEMAS",
+    allDillemas: "ALL_DILLEMAS",
+    getNotess: "GET_NOTES",
+    
+}
+
+export default actionTypes
