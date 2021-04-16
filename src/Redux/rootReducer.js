850eac1c8c83628eaf89aaf48b8b27836ac62f44
Victorsloop <torres.victor136@gmail.com>
2021-04-16 12:05:58 -0400

    Set user function in root reducer

--- src/Redux/rootReducer.js
+++ src/Redux/rootReducer.js
@@ -8,6 +8,21 @@ const defaultState = {
     notes:[]
 }
 
+function setUser(state = defaultState.currentUser, action) {
+    switch (action.type) {
+        case "SET_USER":
+            return (action.payload)
+        case "REFRESH_USER":
+            return (action.payload)
+        case "UPDATE_USER":
+            return (action.payload)
+        case "DELETE_USER":
+            return []
+        default:
+            return state
+    }
+}
+
 const rootReducer = combineReducers({
     currentUser: setUser,
     allDillemas: getDillemasData,
