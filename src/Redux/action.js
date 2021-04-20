b6b06af0a95fd71e1806a238ad8217565b356afe
Victorsloop <torres.victor136@gmail.com>
2021-04-20 12:03:38 -0400

    Login function for setUser in actions

---
+++ src/Redux/action.js
@@ -0,0 +1,23 @@
+import actionTypes from './actionTypes'
+
+export function setUser(userObj, history) {
+    return function (dispatch) {
+        fetch('http://localhost:3000/api/v1/login', {
+            method: "POST",
+            headers: {
+                Accepts: "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ user: userObj })
+        })
+            .then(response => response.json())
+            .then(userData => {
+                console.log("IN SET USER 2", userData)
+                localStorage.setItem("token", userData.jwt)
+                localStorage.setItem("user_id", userData.user.id)
+                dispatch({ type: actionTypes.currentUser, payload: userData })
+                history.push("/home")
+            })
+            .catch(console.log)
+    }
+}
