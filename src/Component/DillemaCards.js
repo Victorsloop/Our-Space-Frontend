46386203a88233c8d5967e5a62382ee3163e0c33
Victorsloop <torres.victor136@gmail.com>
2021-04-22 13:08:41 -0400

    Missed information

--- src/Component/DillemaCards.js
+++ src/Component/DillemaCards.js
@@ -4,7 +4,14 @@ import {Navlink} from 'react-router-dom'
 class DillemaCards extends React.Component{
     render(){
         return(
+            <>
             <h1>Dillema Card Layout</h1>
+            <div class ="card">
+                <h2>Title:</h2>
+                <h3>Content:</h3>
+
+            </div>
+            </>
         )
     }
 }
