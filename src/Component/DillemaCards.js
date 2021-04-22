ffe8556a7968fe2de49f420a8d6ac9f820513832
Victorsloop <torres.victor136@gmail.com>
2021-04-22 13:05:51 -0400

    Dillema card basic layout

---
+++ src/Component/DillemaCards.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Navlink} from 'react-router-dom'
+
+class DillemaCards extends React.Component{
+    render(){
+        return(
+            <h1>Dillema Card Layout</h1>
+        )
+    }
+}
+export default DillemaCards
