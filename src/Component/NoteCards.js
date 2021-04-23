7ff4656339d03ce0e699e89cc7a8f58ac2ae9bab
Victorsloop <torres.victor136@gmail.com>
2021-04-23 12:18:06 -0400

    Note card layout

---
+++ src/Component/NoteCards.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Navlink} from 'react-router-dom'
+
+class NoteCards extends React.Component{
+    render(){
+        return(
+            <>
+            <h1>NoteCards Card Layout</h1>
+            <div class ="card">
+                <h2>Title:</h2>
+                <h3>Content:</h3>
+
+            </div>
+            </>
+        )
+    }
+}
+export default NoteCards
