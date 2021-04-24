856083cbbc51d9e8cfbdb91ac91a9117488ae6f6
Victorsloop <torres.victor136@gmail.com>
2021-04-24 14:31:18 -0400

    Search layout

---
+++ src/Container/SearchDillemas.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {Navlink} from 'react-router-dom'
+
+class SearchDillema extends React.Component {
+
+    render(){
+        return(
+
+            <h1>This is for searching the dillema </h1>
+        )
+    }
+}
+
+
+
+export default connect(SearchDillema)
