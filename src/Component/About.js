1b53e2b835b86efea0c937ceaa5999ec676b0b4a
Victorsloop <torres.victor136@gmail.com>
2021-04-25 11:08:14 -0400

    About page layout

---
+++ src/Component/About.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {Navlink} from 'react-router-dom'
+
+class About extends React.Component {
+
+    render(){
+        return(
+
+            <h1>This is the about page </h1>
+        )
+    }
+}
+
+function msp(state) {
+    return ({
+
+    })
+}
+
+function mdp(dispatch) {
+    return {
+    }
+}
+
+
+export default connect(msp,mdp)(About)
