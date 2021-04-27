480ec4489fd8b5f395034c4492c8d403212bf6b1
Victorsloop <torres.victor136@gmail.com>
2021-04-27 18:06:34 -0400

    Edit profile layot

---
+++ src/Component/EditProfile.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {Navlink} from 'react-router-dom'
+
+class EditProfile extends React.Component{
+
+    render(){
+        return(
+            <h1>Your profile Page</h1>
+        )
+    }
+}
+
+function msp(state){
+    return({
+        curentUser: state.curentUser
+    })
+}
+
+function mdp(dispatch){
+    return({
+        
+    })
+}
+
+export default connect(msp,mdp)(EditProfile)
