bc39f1b4cf46aaccd215bba3c9b74adbd1b45795
Victorsloop <torres.victor136@gmail.com>
2021-05-17 12:24:02 -0400

    Basic inbox layout

---
+++ src/Component/Inbox.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Navlink} from 'react-router-dom'
+
+class Inbox extends React.Component {
+
+    render(){
+        return(
+
+            <h1>This is the inbox page </h1>
+        )
+    }
+}
+
+
+
+export default (Inbox)
