b1851a17abec4aedf97ee1c3f3dbbca286071ad9
Victorsloop <torres.victor136@gmail.com>
2021-05-20 12:32:23 -0400

    More storage imports

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import Provider from 'react-redux'
 import BrowserRouter from 'react-router-dom'
 import { PersistGate } from 'redux-persist/lib/integration/react'
 import storage from 'redux-persist/lib/storage'
+import { persistStore, persistReducer } from 'redux-persist'
 const store = createStore( applyMiddleware(thunk))
 
 ReactDOM.render(
